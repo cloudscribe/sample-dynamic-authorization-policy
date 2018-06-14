f78bf43d7c59afa9951f222335d13b06db4e3f88
Joe Audette <joe.audette@gmail.com>
2018-06-14 13:56:16 -0400

    generate cloudscribe demo project

---
+++ cloudscribe-sample/cloudscribe.DemoWeb/cloudscribe.DemoWeb/Startup.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+
+
+namespace cloudscribe.DemoWeb
+{
+    public class Startup
+    {
+        public Startup(
+            IConfiguration configuration, 
+            IHostingEnvironment env,
+            ILogger<Startup> logger
+            )
+        {
+            _configuration = configuration;
+            _environment = env;
+            _log = logger;
+
+            _sslIsAvailable = _configuration.GetValue<bool>("AppSettings:UseSsl");
+            _disableIdentityServer = _configuration.GetValue<bool>("AppSettings:DisableIdentityServer");
+        }
+
+        private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _environment;
+        private readonly bool _sslIsAvailable;
+        private readonly bool _disableIdentityServer;
+        private bool _didSetupIdServer = false;
+        private readonly ILogger _log;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //// **** VERY IMPORTANT *****
+            // This is a custom extension method in Config/DataProtection.cs
+            // These settings require your review to correctly configur data protection for your environment
+            services.SetupDataProtection(_configuration, _environment);
+            
+            services.AddAuthorization(options =>
+            {
+                //https://docs.asp.net/en/latest/security/authorization/policies.html
+                //** IMPORTANT ***
+                //This is a custom extension method in Config/Authorization.cs
+                //That is where you can review or customize or add additional authorization policies
+                options.SetupAuthorizationPolicies();
+
+            });
+
+            //// **** IMPORTANT *****
+            // This is a custom extension method in Config/CloudscribeFeatures.cs
+            services.SetupDataStorage(_configuration);
+            
+            //*** Important ***
+            // This is a custom extension method in Config/IdentityServerIntegration.cs
+            // You should review this and understand what it does before deploying to production
+            services.SetupIdentityServerIntegrationAndCORSPolicy(
+                _configuration,
+                _environment,
+                _log,
+                _sslIsAvailable,
+                _disableIdentityServer,
+                out _didSetupIdServer
+                );
+
+            //*** Important ***
+            // This is a custom extension method in Config/CloudscribeFeatures.cs
+            services.SetupCloudscribeFeatures(_configuration);
+
+            //*** Important ***
+            // This is a custom extension method in Config/Localization.cs
+            services.SetupLocalization();
+
+            //*** Important ***
+            // This is a custom extension method in Config/RoutingAndMvc.cs
+            services.SetupMvc(_sslIsAvailable);
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            IOptions<cloudscribe.Core.Models.MultiTenantOptions> multiTenantOptionsAccessor,
+            IOptions<RequestLocalizationOptions> localizationOptionsAccessor
+            )
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/oops/error");
+            }
+
+            app.UseForwardedHeaders();
+            app.UseStaticFiles();
+
+            app.UseRequestLocalization(localizationOptionsAccessor.Value);
+            app.UseCors("default"); //use Cors with policy named default, defined above
+
+            var multiTenantOptions = multiTenantOptionsAccessor.Value;
+
+            app.UseCloudscribeCore(
+                    loggerFactory,
+                    multiTenantOptions,
+                    _sslIsAvailable);
+
+            if (!_disableIdentityServer && _didSetupIdServer)
+            {
+                try
+                {
+                    app.UseIdentityServer();
+                }
+                catch(Exception ex)
+                {
+                    _log.LogError($"failed to setup identityserver4 {ex.Message} {ex.StackTrace}");
+                }
+            }
+
+            app.UseMvc(routes =>
+            {
+                var useFolders = multiTenantOptions.Mode == cloudscribe.Core.Models.MultiTenantMode.FolderName;
+                //*** IMPORTANT ***
+                // this is in Config/RoutingAndMvc.cs
+                // you can change or add routes there
+                routes.UseCustomRoutes(useFolders);
+            });
+   
+        }
+
+        
+        
+    }
+}
